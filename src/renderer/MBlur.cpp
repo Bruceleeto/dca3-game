dac22aedf0226f4a6c9d01b4111edcdc976b3108
Josh Pearson <josh@angulartrading.com>
2025-03-04 11:09:45 -0700

    FSAA - Explicit typecast for sim-clang build
    error: type 'float' cannot be narrowed to 'rw::int32'

--- src/renderer/MBlur.cpp
+++ src/renderer/MBlur.cpp
@@ -134,7 +134,7 @@ CMBlur::MotionBlurOpen(RwCamera *cam)
 	
 	return TRUE;
 #else
-	RwRect rect = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+	RwRect rect = { 0, 0, (rw::int32)SCREEN_WIDTH, (rw::int32)SCREEN_HEIGHT };
 	CreateImmediateModeData(cam, &rect);
 	return TRUE;
 #endif
