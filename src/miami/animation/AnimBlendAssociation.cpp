7d77bacf55a83efc825bba6b0f8a3844bdefb86b
Stefanos Kornilios Mitsis Poiitidis <skmp@emudev.org>
2025-03-09 22:15:46 +0200

    Fix miami build

--- src/miami/animation/AnimBlendAssociation.cpp
+++ src/miami/animation/AnimBlendAssociation.cpp
@@ -84,7 +84,6 @@ CAnimBlendAssociation::Init(RpClump *clump, CAnimBlendHierarchy *hier)
 			frame = RpAnimBlendClumpFindBone(clump, seq->boneTag);
 		if(frame && seq->numFrames > 0) {
 			nodes[frame - clumpData->frames].sequence = seq;
-			nodes[frame - clumpData->frames].Setup();
 		}
 	}
 }
