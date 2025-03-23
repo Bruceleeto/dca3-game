3b7f5cffc3db945a2c8ea4acbf85fa28ebfe775c
Stefanos Kornilios Mitsis Poiitidis <skmp@nilware.io>
2025-03-23 07:39:22 +0200

    collision fixes: fix miami linked list corruption, liberty replica

--- src/liberty/modelinfo/BaseModelInfo.h
+++ src/liberty/modelinfo/BaseModelInfo.h
@@ -56,8 +56,7 @@ public:
 	}
 	char *GetModelName(void) { return m_name; }
 	void SetModelName(const char *name) { strncpy(m_name, name, MAX_MODEL_NAME); }
-	void SetColModel(CColModel *col, bool owns = false){
-		m_colModel = col; m_bOwnsColModel = owns; }
+	void SetColModel(CColModel *col, bool owns = false);
 	CColModel *GetColModel(void) { return m_colModel; }
 	bool DoesOwnColModel(void) { return m_bOwnsColModel; }
 	void DeleteCollisionModel(void);
